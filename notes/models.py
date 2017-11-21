1a3d412b9d9f072cc4db515bdbccac4001938d6b
Bryan Sibaja <bryan.sibaja@ucr.ac.cr>
2017-11-20 18:46:53 -0600

    first commit

---
+++ notes/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Libro(models.Model):
+    nombre = models.CharField(null=False, max_length=35)
+    padre = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
+    pertenece = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+
+class Nota(models.Model):
+    titulo = models.CharField(max_length = 35, null=False)
+    cuerpo = models.TextField(null=False)
+    creacion = models.DateField(null=False)
+    actualizacion = models.DateField(null=False)
+    borrado = models.BooleanField()
+    libro = models.ForeignKey(Libro, null=False, on_delete=models.CASCADE)
+    compartido = models.ManyToManyField(User)
+
+class Etiquetas(models.Model):
+    nombre = models.CharField(null=False, max_length=35)
+    nota = models.ForeignKey(Nota, null=False, on_delete=models.CASCADE)
+
+class Imagen(models.Model):
+    imagen = models.ImageField(null=False)
+    usuario = models.ForeignKey(User, null=False)
